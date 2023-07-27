48e8ba7a03e80c67ee9474b726675d0468b00d5a
suji5262 <suji5262@gmail.com>
2023-07-27 15:40:35 +0900

    :sparkles: SpringConfig file add

--- Spring_Basic/hello-spring/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ Spring_Basic/hello-spring/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,21 +1,30 @@
 package hello.hellospring;
-
+import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-//@Configuration
+import javax.sql.DataSource;
+@Configuration
 public class SpringConfig {
-
+    private final DataSource dataSource;
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
     } // 위와 같이 연결 시킴
 
+//    @Bean
+//    public MemberRepository memberRepository() {
+//        return new MemoryMemberRepository();
+//    } // 인터페이스는 new 가 안됨
+
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
-    } // 인터페이스는 new 가 안됨
+// return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
+    }
 }
